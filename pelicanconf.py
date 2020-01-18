5555048484e5c56372670d2df6b0ef1f321f52f8
Sara Frank <saraf@CORPORATE.inf-grp.com>
2020-01-17 19:41:12 -0600

    First commit.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'casef'
-SITENAME = 'My Blog'
+AUTHOR = 'TCMG-316-GROUP1'
+SITENAME = 'Aggie Life'
 SITEURL = ''
 
 PATH = 'content'
@@ -11,6 +11,8 @@ PATH = 'content'
 TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = 'en'
+
+IGNORE_FILES = ['_*.*']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +22,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Texas A&M University Events', 'https://calendar.tamu.edu/'),
+         ('Texags', 'http://texags.com'),
+         ('Aggie Traditions', 'https://www.tamu.edu/traditions/'),
+         ('TAMU Student Life', 'https://www.tamu.edu/student-life/index.html'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('@TAMU', 'https://twitter.com/TAMU'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
